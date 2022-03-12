b48075faa1e7a1d7beb4f5a495f13d03b3b68fd7
hiro881213 <nikep.godbard@gmail.com>
2022-03-12 17:38:02 +0900

    setting BackgroundColor

--- src/App.tsx
+++ src/App.tsx
@@ -1,89 +1,65 @@
 import React from 'react';
 import Particles from "react-tsparticles";
+import ParticlesParams from './assets/particles-default.json';
 import logo from './logo.svg';
 import './App.scss';
 import classes from "./Particle.module.scss";
 
 function App() {
+
+  const params = ParticlesParams as typeof ParticlesParams;
+
   return (
     <div className="App">
-    
-    
-    <Particles
-      id="tsparticles"
-      options={{
-        background: {
-          color: {
-            value: "#0d47a1",
-          },
-        },
-        fpsLimit: 120,
-        interactivity: {
-          events: {
-            resize: true,
-          },
-          modes: {
-            bubble: {
-              distance: 400,
-              duration: 2,
-              opacity: 0.8,
-              size: 40,
-            },
-            push: {
-              quantity: 4,
-            },
-            repulse: {
-              distance: 200,
-              duration: 0.4,
-            },
-          },
-        },
-        particles: {
-          color: {
-            value: "#ffffff",
-          },
-          links: {
-            color: "#ffffff",
-            distance: 150,
-            enable: true,
-            opacity: 0.5,
-            width: 1,
-          },
-          collisions: {
-            enable: true,
-          },
-          move: {
-            direction: "none",
-            enable: true,
-            outMode: "bounce",
-            random: false,
-            speed: 6,
-            straight: false,
-          },
-          number: {
-            density: {
-              enable: true,
-              area: 800,
-            },
-            value: 80,
-          },
-          opacity: {
-            value: 0.5,
-          },
-          shape: {
-            type: "circle",
-          },
-          size: {
-            random: true,
-            value: 5,
-          },
-        },
-        detectRetina: true,
-      }}
-    />
-    
+      <Particles
+        className={classes.Particle}
+        options = {options}
+        // options={params as any} 
+      />
     </div>
   );
 }
+
+export const options = {
+  fpsLimit: 40,
+  particles: {
+      number: {
+          value: 80,
+          density: {
+              enable: true,
+              area: 800,
+          },
+      },
+      color: {
+          value: ["#fdcf58", "#757676", "#f27d0c", "#800909", "#f07f13"],
+      },
+      opacity: {
+          value: 0.5,
+          random: true,
+      },
+      size: {
+          value: 5,
+          random: true,
+      },
+      move: {
+          enable: true,
+          speed: 6,
+          random: false,
+      },
+  },
+  interactivity: {
+      events: {
+          onClick: {
+              enable: true,
+              mode: "push",
+          },
+          resize: true,
+      },
+  },
+  background: {
+      image: "radial-gradient(#222, #000)",
+  },
+};
+
 
 export default App;
