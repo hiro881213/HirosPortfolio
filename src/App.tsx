71f122bf903675dc7e350cac294f4c3490c41c49
hiro881213 <nikep.godbard@gmail.com>
2022-03-12 18:01:56 +0900

    setting Particle JSON

--- src/App.tsx
+++ src/App.tsx
@@ -13,53 +13,11 @@ function App() {
     <div className="App">
       <Particles
         className={classes.Particle}
-        options = {options}
+        options = {params}
         // options={params as any} 
       />
     </div>
   );
 }
-
-export const options = {
-  fpsLimit: 40,
-  particles: {
-      number: {
-          value: 80,
-          density: {
-              enable: true,
-              area: 800,
-          },
-      },
-      color: {
-          value: ["#fdcf58", "#757676", "#f27d0c", "#800909", "#f07f13"],
-      },
-      opacity: {
-          value: 0.5,
-          random: true,
-      },
-      size: {
-          value: 5,
-          random: true,
-      },
-      move: {
-          enable: true,
-          speed: 6,
-          random: false,
-      },
-  },
-  interactivity: {
-      events: {
-          onClick: {
-              enable: true,
-              mode: "push",
-          },
-          resize: true,
-      },
-  },
-  background: {
-      image: "radial-gradient(#222, #000)",
-  },
-};
-
 
 export default App;
