8dafce0ed59ea42547d7c6f682f638f7b6741d02
hiro881213 <nikep.godbard@gmail.com>
2022-03-13 17:03:15 +0900

    Menu Making

--- src/components/Home/Menu.tsx
+++ src/components/Home/Menu.tsx
@@ -1,17 +1,19 @@
 import type { VFC } from "react";
 import { MenuItem } from './MenuItem';
 import classes from './scss/Menu.module.scss';
-import scriptImageUrl from "../../assets/img/ScriptImage.jpg";
 
+import scriptImageUrl from "../../assets/img/ScriptImage.jpg";
+import graphicImageUrl from "../../assets/img/GraphicImage.jpg";
+import programImageUrl from "../../assets/img/ProgramImage.jpg";
 
 export const Menu: VFC = () => {
 
     return(
         <>  
             <div className={classes.contents}>
-                <MenuItem image = {scriptImageUrl}>Script</MenuItem>
-                {/* <MenuItem>Graphic</MenuItem>
-                <MenuItem>Programming</MenuItem> */}
+                <MenuItem image = {scriptImageUrl} marginContents = "0px">Script</MenuItem>
+                <MenuItem image = {graphicImageUrl} marginContents = "60px">Graphic</MenuItem>
+                <MenuItem image = {programImageUrl} marginContents = "60px">Programming</MenuItem>
             </div>
         </>
     )
--- src/components/Home/MenuItem.tsx
+++ src/components/Home/MenuItem.tsx
@@ -2,9 +2,14 @@ import classes from './scss/Menu.module.scss';
 
 export const MenuItem = (props: any) => {
 
+    const style = {
+        marginLeft: props.marginContents,
+    };
+
+
     return (
         <>
-            <div className={classes.item} >
+            <div className={classes.item} style ={style}>
                 <span>{props.children}</span>
                 <img src={props.image} alt="picture" />
             </div>
