6e9070261de36db6f3a863ee2e1f7ede9b312593
hiro881213 <nikep.godbard@gmail.com>
2022-03-13 19:08:25 +0900

    Changing Particle

--- src/components/Home/HomeParticles.tsx
+++ src/components/Home/HomeParticles.tsx
@@ -2,12 +2,38 @@ import type { VFC } from "react";
 import Particles from "react-tsparticles";
 import classes from "./scss/Particle.module.scss";
 import ParticlesParams from '../../assets/particles-default.json';
+import ParticlesParams2 from '../../assets/particles-default2.json';
+import ParticlesParams3 from '../../assets/particles-default3.json';
+
 
 export const HomeParticles: VFC = () => {
 
-    const params = ParticlesParams as typeof ParticlesParams;
+    // 乱数を生成する
+    const min = 1 ;
+    const max = 10 ;
 
-    return (      
+    const randomVal = Math.floor( Math.random() * (max + 1 - min) ) + min ;
+
+    let targetJSON;
+
+    if (randomVal <= 3) {
+
+        targetJSON = ParticlesParams;
+
+    } else if  (randomVal <= 6 && randomVal > 3) {
+
+        targetJSON = ParticlesParams2;
+
+    } else {
+
+        targetJSON = ParticlesParams3;
+
+    }
+
+    // 背景色を設定する
+    const params = targetJSON;
+
+    return (
         <>
             <Particles
                 className={classes.Particle}
