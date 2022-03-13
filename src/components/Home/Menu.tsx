e7b8d88e5e149b09e7c8174f13090050c9f9aeb1
hiro881213 <nikep.godbard@gmail.com>
2022-03-13 16:23:31 +0900

    Design Menu

--- src/components/Home/Menu.tsx
+++ src/components/Home/Menu.tsx
@@ -1,15 +1,17 @@
 import type { VFC } from "react";
 import { MenuItem } from './MenuItem';
 import classes from './scss/Menu.module.scss';
+import scriptImageUrl from "../../assets/img/ScriptImage.jpg";
+
 
 export const Menu: VFC = () => {
 
     return(
         <>  
             <div className={classes.contents}>
-                <MenuItem>Script</MenuItem>
-                <MenuItem>Graphic</MenuItem>
-                <MenuItem>Programming</MenuItem>
+                <MenuItem image = {scriptImageUrl}>Script</MenuItem>
+                {/* <MenuItem>Graphic</MenuItem>
+                <MenuItem>Programming</MenuItem> */}
             </div>
         </>
     )
--- src/components/Home/MenuItem.tsx
+++ src/components/Home/MenuItem.tsx
@@ -1,10 +1,12 @@
 import classes from './scss/Menu.module.scss';
 
 export const MenuItem = (props: any) => {
+
     return (
         <>
-            <div className={classes.item}>
+            <div className={classes.item} >
                 <span>{props.children}</span>
+                <img src={props.image} alt="picture" />
             </div>
         </>
     );
