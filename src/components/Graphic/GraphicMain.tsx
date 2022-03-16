3bbc3017ad7f58f52569bd145e37327dc01fdcf5
hiro881213 <nikep.godbard@gmail.com>
2022-03-17 02:27:56 +0900

    Programming Graphic Page

--- src/components/Graphic/GraphicMain.tsx
+++ src/components/Graphic/GraphicMain.tsx
@@ -1,7 +1,40 @@
+import { Link } from 'react-router-dom';
+
+import classes from './scss/GraphicMain.module.scss';
+import IMG1 from '../../assets/img/ProgramImage.jpg'
+import IMG2 from '../../assets/img/GraphicImage.jpg'
+import IMG3 from '../../assets/img/ScriptImage.jpg'
+
+
 export const GraphicMain = () => {
     return (
         <>
-            <label>BBBB</label>
+            <header className = {classes.headerClass}>
+                <div className = {classes.title}>
+                    <p className = {classes.titleMain}>My Graphic Designs</p>
+                    <label>〜PhotoShop Work And CG Art Work〜</label>
+                </div>
+            </header>
+
+            <main className = {classes.mainClass}>
+                <div className = {classes.worksArea}>
+                    <div>
+                        <img src= {IMG1} alt="picture" />
+                    </div>
+
+                    <div>
+                        <img src= {IMG2} alt="picture" />
+                    </div>
+
+                    <div>
+                        <img src= {IMG3} alt="picture" />
+                    </div>
+
+                </div>
+            </main>
+            <footer>
+                <Link to = '/'>HOME</Link>
+            </footer>
         </>
     );
 }
