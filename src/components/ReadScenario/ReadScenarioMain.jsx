1eae4c689fe3828c4bd038accc3e0e2fbbf0bf4d
hiro881213 <nikep.godbard@gmail.com>
2022-03-26 14:39:43 +0900

    Page Setting

--- src/components/ReadScenario/ReadScenarioMain.jsx
+++ src/components/ReadScenario/ReadScenarioMain.jsx
@@ -1,81 +1,167 @@
-import React from 'react'
-import { useLocation } from 'react-router-dom';
-import HTMLFlipBook from "react-pageflip";
-import { Link } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import $ from "jquery";
+import "turn.js";
 
-import classes from './scss/ReadScenarioMain.module.scss'
+import "./styles.css";
 
-// シナリオデータ取得・表示処理
-export const ReadScenarioMain = (props) => {
+class Turn extends React.Component {
 
-    // GETパラメータを取得する
-    const params = new URLSearchParams(useLocation().search);
-    const target = params.get('story')
-
-    // フォルダ内ファイル取得処理
-    const importAll = (r)  => {
-        return r.keys().map(r);
+    static defaultProps = {
+        style: {},
+        className: "",
+        options: {}
     };
-    
-    // フォルダ内ファイルインポート処理
-    function componentWillMount() {
 
-        let lstImages = [];
-
-        if (target === '1') {
-            lstImages = importAll(require.context('../../assets/scenario/story01/', true, /\.(jpg)$/));
-
-        } else if (target === '2') {
-            lstImages = importAll(require.context('../../assets/scenario/story2/', true, /\.(jpg)$/));
-            console.log(lstImages);
+    componentDidMount = () => {
+        
+        if (this.el) {
+            $(this.el).turn(Object.assign({}, this.props.options));
         }
+        
+        // キーダウンイベントを追加
+        document.addEventListener("keydown", this.handleKeyDown, false);
+        
+        // Nextボタンクリックイベントを追加
+        document.getElementById('next').addEventListener("click",this.handleNextButton, false);
 
-        return lstImages;
+        // Prevボタンクリックイベントを追加
+        document.getElementById('prev').addEventListener("click", this.handleprevButton, false);
+
+        // 先頭ボタンクリックイベントを追加
+        document.getElementById('first').addEventListener("click", this.handleFirstButton, false);
+
 
     }
 
-    // 脚本データを取得する
-    const listOfImages = componentWillMount();
+    componentWillUnmount = () => {
 
-    // Pageを定義する
-    const Page = React.forwardRef((props, ref={ref}) => {
+        if (this.el) {
+            $(this.el).turn("destroy").remove();
+        }
+
+        // キーダウンイベントをアンマウント
+        document.removeEventListener("keydown", this.handleKeyDown, false);
+        
+        //　Nextボタンクリックイベントをアンマウント
+        document.getElementById('next').removeEventListener("click",this.handleNextButton, false);
+
+        // Prevボタンクリックイベントをアンマウント
+        document.getElementById('prev').removeEventListener("click", this.handleprevButton, false);
+
+    }
+
+    // ----------------------------------------
+    // Prevボタン押下処理
+    // ----------------------------------------
+
+    handleKeyDown = event => {
+        if (event.keyCode === 37) {
+            $(this.el).turn("previous");
+        }
+    
+        if (event.keyCode === 39) {
+            $(this.el).turn("next");
+        }
+    };
+
+    // ----------------------------------------
+    // Nextボタン押下処理
+    // ----------------------------------------
+
+    handleNextButton = () => {
+        $(this.el).turn("previous");
+    }
+
+    // ----------------------------------------
+    // Prevボタン押下処理
+    // ----------------------------------------
+
+    handleprevButton = () => {
+        $(this.el).turn("next");
+    }
+
+    // ----------------------------------------
+    // 先頭ボタン押下処理
+    // ----------------------------------------
+
+    handleFirstButton = () => {
+        $(this.el).turn('page',6);
+    }
+
+    render() {
         return (
-            <> 
-                <div>
-                    <img src = {props.page} />
-                </div>
-            </>
+        <>
+            <div
+                className={this.props.className}
+                style={Object.assign({}, this.props.style)}
+                ref={el => (this.el = el)}>
+                {this.props.children}
+            </div>
+        </>
         );
-    });
+    }
+
+}
+
+const options = {
+    width: 800,
+    height: 600,
+    autoCenter: true,
+    display: "double",
+    acceleration: true,
+    elevation: 50,
+    gradients: !$.isTouch,
+    when: {
+        turned: function(e, page) {
+        console.log("Current view: ", $(this).turn("view"));
+        }
+    },
+    // direction: 'ltr'
+    direction: "rtl"
+};
+
+const pages = [
+    "https://raw.github.com/blasten/turn.js/master/demos/magazine/pages/01.jpg",
+    "https://raw.github.com/blasten/turn.js/master/demos/magazine/pages/02.jpg",
+    "https://raw.github.com/blasten/turn.js/master/demos/magazine/pages/03.jpg",
+    "https://raw.github.com/blasten/turn.js/master/demos/magazine/pages/04.jpg",
+    "https://raw.github.com/blasten/turn.js/master/demos/magazine/pages/05.jpg",
+    "https://raw.github.com/blasten/turn.js/master/demos/magazine/pages/06.jpg"
+];
+
+// ------------------------------------------------
+// ページ読み込み処理
+// ------------------------------------------------
+
+export const ReadScenarioMain = () => {
 
     return (
         <>
-            <main className={classes.readScinario}>
-                <div>
-                    <HTMLFlipBook width={800} height={500} drawShadow = {true}>
-                    {
-                        (() => {
+            {
+                // オンロード関数
+                window.onload = function() {
 
-                            const pagesElements = [];
+                    // ページを先頭に移動
+                    document.getElementById('first').click();
 
-                            // 対象ファイルを表示する
-                            listOfImages.forEach((page) => {
-                                pagesElements.push(<Page page = {page}></Page>);
-                            });
+                }
 
-                            return pagesElements;
+            }
 
-                        })()
-                    }
-                    </HTMLFlipBook>
-
-                    <div className={classes.button}>
-                        <Link to = '../script'>
-                            <button>Go Back</button>
-                        </Link>
+            <Turn  options={options} className="magazine">
+                {pages.map((page, index) => (
+                    <div key={index} className="page">
+                        <img src={page} alt="" />
                     </div>
-                </div>
-            </main>
+                ))}
+            </Turn>
+
+            <button id = 'next'>⇦⇦Next</button>
+            <button id = 'prev'>Prev⇨⇨</button>
+            <button id = 'first'>先頭</button>
+            
+            
         </>
     );
-}
+};
