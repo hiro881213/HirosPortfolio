54927f2362b576fd1474afcb1ffe62e2ec51b0d2
hiro881213 <nikep.godbard@gmail.com>
2022-04-11 09:08:02 +0900

    メディアクエリ導入

--- src/components/BaseArea/BaseApp.tsx
+++ src/components/BaseArea/BaseApp.tsx
@@ -1,3 +1,5 @@
+import MediaQuery from "react-responsive";
+
 import { HomeOpen } from './HomeOpen';
 import { HomeParticles } from './HomeParticles';
 import { HeaderData } from '../HeaderArea/HeaderData';
@@ -8,17 +10,34 @@ export const BaseApp = () => {
 
     return (
         <>
-            <HomeOpen />
-            <HomeParticles />
-            <header>
-                <HeaderData></HeaderData>
-            </header>
 
-            <section className = 'menuList'>
-                <div className = 'w-container' style = {{height: '100%'}}>
-                    <MenuList></MenuList>
-                </div>
-            </section>
+            <HomeOpen />
+            
+            {/* <!-- スマートフォン向け画面レイアウト --> */}
+            <MediaQuery query = '(max-width: 767px)'>
+                <header>
+                    <HeaderData></HeaderData>
+                </header>
+                <section className = 'menuList'>
+                    <div className = 'w-container' style = {{height: '100%'}}>
+                        <MenuList></MenuList>
+                    </div>
+                </section>
+            </MediaQuery>
+
+            {/* <!-- PC向け画面レイアウト --> */}
+            <MediaQuery query = '(min-width: 768px)'>
+                <HomeParticles />
+                <header>
+                    <HeaderData></HeaderData>
+                </header>
+                <section className = 'menuList'>
+                    <div className = 'w-container' style = {{height: '100%'}}>
+                        <MenuList></MenuList>
+                    </div>
+                </section>
+            </MediaQuery>
+
 
             {/* <footer></footer> */}
         </>
