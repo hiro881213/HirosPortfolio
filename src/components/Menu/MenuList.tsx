cf7b8a43574feeff5ee86a48d4d9e2cb2d197d40
hiro881213 <nikep.godbard@gmail.com>
2022-04-16 20:23:44 +0900

    Make SmartPhone Component

--- src/components/Menu/MenuList.tsx
+++ src/components/Menu/MenuList.tsx
@@ -2,6 +2,7 @@ import MediaQuery from "react-responsive";
 import './scss/MenuList.scss';
 
 import { MenuItem } from './MenuItem';
+import { SmartProfile } from "./SmartProfile";
 
 import program from '../../assets/img/ProgramImage.jpg';
 import script  from '../../assets/img/ScriptImage.jpg';
@@ -17,22 +18,7 @@ export const MenuList = () => {
                     <article className = 'post'>
                         <h2>Road Of Contents Creator</h2>
                         <MediaQuery query = '(max-width: 767px)'>
-                            <h3>Name:</h3>
-                            <h3 style = {{paddingLeft: '20px'}}>F.T.Hiro</h3>
-
-                            <h3>Degree:</h3>
-                            <h3 style = {{paddingLeft: '20px'}}>Master of Engineering</h3>
-
-                            <h3>Career:</h3>
-                            <h3 style = {{paddingLeft: '20px'}}>System Engineer In SIer Company</h3>
-
-                            <h3>Position:</h3>
-                            <h3 style = {{paddingLeft: '20px'}}>Full Stack</h3>
-
-                            <h3>Other Skill:</h3>
-                            <h3 style = {{paddingLeft: '20px'}}>Scenario writing</h3>
-                            <h3 style = {{paddingLeft: '20px'}}>Video Production</h3>
-                            <h3 style = {{paddingLeft: '20px'}}>Video Editing</h3>
+                            <SmartProfile></SmartProfile>
                         </MediaQuery>
 
                         <MediaQuery query = '(min-width: 768px)'>
---
+++ src/components/Menu/SmartProfile.tsx
@@ -0,0 +1,27 @@
+export const SmartProfile = () => {
+
+    const padding = {
+        paddingLeft: '20px'
+    };
+
+    return (
+        <>
+            <h3>Name:</h3>
+            <h3 style = {padding}>F.T.Hiro</h3>
+
+            <h3>Degree:</h3>
+            <h3 style = {padding}>Master of Engineering</h3>
+
+            <h3>Career:</h3>
+            <h3 style = {padding}>System Engineer In SIer Company</h3>
+
+            <h3>Position:</h3>
+            <h3 style = {padding}>Full Stack</h3>
+
+            <h3>Other Skill:</h3>
+            <h3 style = {padding}>Scenario writing</h3>
+            <h3 style = {padding}>Video Production</h3>
+            <h3 style = {padding}>Video Editing</h3>
+        </>
+    );
+}
