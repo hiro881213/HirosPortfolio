5ad17657e51adb8e1e092c88fa4bb7927ede7ae0
hiro881213 <nikep.godbard@gmail.com>
2022-04-16 20:36:25 +0900

    Make PC image Component

--- src/components/Menu/MenuList.tsx
+++ src/components/Menu/MenuList.tsx
@@ -3,6 +3,7 @@ import './scss/MenuList.scss';
 
 import { MenuItem } from './MenuItem';
 import { SmartProfile } from "./SmartProfile";
+import { PcProfile } from "./PcProfile";
 
 import program from '../../assets/img/ProgramImage.jpg';
 import script  from '../../assets/img/ScriptImage.jpg';
@@ -15,39 +16,27 @@ export const MenuList = () => {
     return (
         <>
             <div className = 'posts-container'>
-                    <article className = 'post'>
-                        <h2>Road Of Contents Creator</h2>
-                        <MediaQuery query = '(max-width: 767px)'>
-                            <SmartProfile></SmartProfile>
-                        </MediaQuery>
-
-                        <MediaQuery query = '(min-width: 768px)'>
-                            <h3>Name: F.T.Hiro</h3>
-
-                            <h3>Degree: Master of Engineering</h3>
-
-                            <h3>Career: System Engineer In SIer Company</h3>
-
-                            <h3>Position: PM & Full Stack Engineer</h3>
-
-                            <h3>Other Skill:</h3>
-                            <h3 style = {{paddingLeft: '20px'}}>Scenario writing</h3>
-                            <h3 style = {{paddingLeft: '20px'}}>Video Production</h3>
-                            <h3 style = {{paddingLeft: '20px'}}>Video Editing</h3>
-
-                        </MediaQuery>
-                    </article>
-
-                    <MediaQuery query = '(min-width: 768px)'>
-                        <article>
-                            <h1>HIRO'S<br/>PORTFOLIO</h1>
-                        </article>
+                <article className = 'post'>
+                    <h2>Road Of Contents Creator</h2>
+                    <MediaQuery query = '(max-width: 767px)'>
+                        <SmartProfile></SmartProfile>
                     </MediaQuery>
 
+                    <MediaQuery query = '(min-width: 768px)'>
+                        <PcProfile></PcProfile>
+                    </MediaQuery>
+                </article>
 
-                    <MenuItem src = {graphic} url = {`${baseURL}graphic`}>graphic</MenuItem>
-                    <MenuItem src = {script}  url = {`${baseURL}script`}>Scenalio</MenuItem>
-                    <MenuItem src = {program}>Programming</MenuItem>
+                <MediaQuery query = '(min-width: 768px)'>
+                    <article>
+                        <h1>HIRO'S<br/>PORTFOLIO</h1>
+                    </article>
+                </MediaQuery>
+
+
+                <MenuItem src = {graphic} url = {`${baseURL}graphic`}>graphic</MenuItem>
+                <MenuItem src = {script}  url = {`${baseURL}script`}>Scenalio</MenuItem>
+                <MenuItem src = {program}>Programming</MenuItem>
             </div>
         </>
     );
---
+++ src/components/Menu/PcProfile.tsx
@@ -0,0 +1,24 @@
+export const PcProfile = () => {
+    
+    const padding = {
+        paddingLeft: '20px'
+    };
+
+    return (
+        <>
+            <h3>Name: F.T.Hiro</h3>
+
+            <h3>Degree: Master of Engineering</h3>
+
+            <h3>Career: System Engineer In SIer Company</h3>
+
+            <h3>Position: PM & Full Stack Engineer</h3>
+
+            <h3>Other Skill:</h3>
+            <h3 style = {padding}>Scenario writing</h3>
+            <h3 style = {padding}>Video Production</h3>
+            <h3 style = {padding}>Video Editing</h3>
+        </>
+    );
+
+}
