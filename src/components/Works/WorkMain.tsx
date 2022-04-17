296b2a644ff783ac1d96602202a66c1cc1626ec4
hiro881213 <nikep.godbard@gmail.com>
2022-04-17 17:54:11 +0900

    First Graphics

--- src/components/Works/WorkMain.tsx
+++ src/components/Works/WorkMain.tsx
@@ -1,5 +1,9 @@
+import { Link } from 'react-router-dom';
+
 import { getUrl } from '../../CommonUtil'
 import { LogoInf } from '../Common/LogoInf'
+import  IMG01 from "../../assets/graphics/IMG01.jpg"
+
 
 import './scss/work.scss';
 
@@ -12,8 +16,21 @@ export const WorkMain = () => {
                     <LogoInf type = {1}></LogoInf>
                 </a>
             </header>
-            <section className = 'workTitle'>
-                <h1>Works</h1>
+            <section className = 'graphics w-container'>
+                <div className = 'workTitle'>
+                    <h1>Works</h1>
+                </div>
+
+                <div className = 'graphicsContainer'>
+                    <article className = 'graphic'>
+                        <Link to = '#'>
+                            <figure>
+                                <img src = {IMG01} alt = 'image'></img>
+                            </figure>
+                        </Link>
+                    </article>
+                </div>
+
             </section>
         </>
     );
