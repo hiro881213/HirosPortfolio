fe45ad03847ab7b0d4c9ad16386389982aeef0a8
hiro881213 <nikep.godbard@gmail.com>
2022-04-23 21:29:24 +0900

    Title set

--- src/components/About/AboutMain.tsx
+++ src/components/About/AboutMain.tsx
@@ -1,7 +1,33 @@
+import { useEffect } from 'react'
+
 import { getUrl } from '../../CommonUtil'
 import { LogoInf } from '../Common/LogoInf'
+import editMovie from '../../assets/about/editMovie.jpg'
+import program from '../../assets/about/programming.jpg'
+import record from '../../assets/about/recording.jpg'
+import settingAudio from '../../assets/about/settingAudio.jpg'
+
+import './scss/About.scss';
+
 
 export const AboutMain = () => {
+
+    const src1  = {
+        backgroundImage: `url(${editMovie})`,
+    };
+    const src2  = {
+        backgroundImage: `url(${program})`,
+        animationDelay: '3s',
+    };
+    const src3  = {
+        backgroundImage: `url(${record})`,
+        animationDelay: '6s',
+    };
+    const src4  = {
+        backgroundImage: `url(${settingAudio})`,
+        animationDelay: '9s',
+    };
+
     return (
         <>
             <header className = 'workContainer w-container'>
@@ -9,6 +35,16 @@ export const AboutMain = () => {
                     <LogoInf type = {1}></LogoInf>
                 </a>
             </header>
+
+            <section className='allAbout'>
+                <div style = {src1} className='bgImg'></div>
+                <div style = {src2} className='bgImg'></div>
+                <div style = {src3} className='bgImg'></div>
+                <div style = {src4} className='bgImg'></div>
+            </section>
+
+            {/* <img src = {image}></img> */}
+
         </>
     )
 }
--- src/components/About/dist/AboutMain.js
+++ src/components/About/dist/AboutMain.js
@@ -3,9 +3,34 @@ exports.__esModule = true;
 exports.AboutMain = void 0;
 var CommonUtil_1 = require("../../CommonUtil");
 var LogoInf_1 = require("../Common/LogoInf");
+var editMovie_jpg_1 = require("../../assets/about/editMovie.jpg");
+var programming_jpg_1 = require("../../assets/about/programming.jpg");
+var recording_jpg_1 = require("../../assets/about/recording.jpg");
+var settingAudio_jpg_1 = require("../../assets/about/settingAudio.jpg");
+require("./scss/About.scss");
 exports.AboutMain = function () {
+    var src1 = {
+        backgroundImage: "url(" + editMovie_jpg_1["default"] + ")"
+    };
+    var src2 = {
+        backgroundImage: "url(" + programming_jpg_1["default"] + ")",
+        animationDelay: '3s'
+    };
+    var src3 = {
+        backgroundImage: "url(" + recording_jpg_1["default"] + ")",
+        animationDelay: '6s'
+    };
+    var src4 = {
+        backgroundImage: "url(" + settingAudio_jpg_1["default"] + ")",
+        animationDelay: '9s'
+    };
     return (React.createElement(React.Fragment, null,
         React.createElement("header", { className: 'workContainer w-container' },
             React.createElement("a", { href: CommonUtil_1.getUrl() },
-                React.createElement(LogoInf_1.LogoInf, { type: 1 })))));
+                React.createElement(LogoInf_1.LogoInf, { type: 1 }))),
+        React.createElement("section", { className: 'allAbout' },
+            React.createElement("div", { style: src1, className: 'bgImg' }),
+            React.createElement("div", { style: src2, className: 'bgImg' }),
+            React.createElement("div", { style: src3, className: 'bgImg' }),
+            React.createElement("div", { style: src4, className: 'bgImg' }))));
 };
