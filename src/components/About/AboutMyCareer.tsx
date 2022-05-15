db56bf78f97083d357d81bbe12a0489f4bdac35c
hiro881213 <nikep.godbard@gmail.com>
2022-05-15 21:55:55 +0900

    add my career animation

--- src/components/About/AboutMyCareer.tsx
+++ src/components/About/AboutMyCareer.tsx
@@ -1,9 +1,17 @@
+import { useInView } from 'react-intersection-observer';
+import 'animate.css';
 import myCareerLabel from '../../assets/textLabels/myCareerLabel.json'
+
 
 export const AboutMyCareer = (props: any) => {
 
+    // -------------------------------------------
+    // ラベル設定処理
+    // -------------------------------------------
+
     const id = props.id;
 
+    // 表示ラベル変数群
     let sysName = "";
     let title = "";
     let first = "";
@@ -12,7 +20,7 @@ export const AboutMyCareer = (props: any) => {
     let fourth = "";
     let five = "";
 
-
+    // キー一覧
     const sysNameKey = `sysName${id}`;
     const titleKey = `title${id}`;
     const firstKey = `first${id}`;
@@ -55,19 +63,31 @@ export const AboutMyCareer = (props: any) => {
 
     }
 
+    const { ref, inView } = useInView({
+        // オプション
+        rootMargin: '-50px', // ref要素が現れてから50px過ぎたら
+        triggerOnce: true, // 最初の一度だけ実行
+    });
+
     return (
         <>
-            <div className='wrap'>
-                <div className='title'><h3>{sysName}</h3></div>
-                <div>
-                    <p>{title}</p>
-                    <p>{first}</p>
-                    <p>{second}</p>
-                    <br></br>
-                    <p>{third}</p>
-                    <p>{fourth}</p>
-                    <p>{five}</p>
-                </div>
+            <div ref={ref} className='wrap'>
+            {
+                inView && (
+                    <div className="animate__animated animate__lightSpeedInLeft">
+                        <div className='title'><h3>{sysName}</h3></div>
+                        <div>
+                            <p>{title}</p>
+                            <p>{first}</p>
+                            <p>{second}</p>
+                            <br></br>
+                            <p>{third}</p>
+                            <p>{fourth}</p>
+                            <p>{five}</p>
+                        </div>
+                    </div>
+                )
+            }
             </div>
         </>
     )
--- src/components/About/dist/AboutMyCareer.js
+++ src/components/About/dist/AboutMyCareer.js
@@ -1,9 +1,15 @@
 "use strict";
 exports.__esModule = true;
 exports.AboutMyCareer = void 0;
+var react_intersection_observer_1 = require("react-intersection-observer");
+require("animate.css");
 var myCareerLabel_json_1 = require("../../assets/textLabels/myCareerLabel.json");
 exports.AboutMyCareer = function (props) {
+    // -------------------------------------------
+    // ラベル設定処理
+    // -------------------------------------------
     var id = props.id;
+    // 表示ラベル変数群
     var sysName = "";
     var title = "";
     var first = "";
@@ -11,6 +17,7 @@ exports.AboutMyCareer = function (props) {
     var third = "";
     var fourth = "";
     var five = "";
+    // キー一覧
     var sysNameKey = "sysName" + id;
     var titleKey = "title" + id;
     var firstKey = "first" + id;
@@ -42,8 +49,13 @@ exports.AboutMyCareer = function (props) {
             five = myCareerLabel_json_1["default"][key];
         }
     }
+    var _a = react_intersection_observer_1.useInView({
+        // オプション
+        rootMargin: '-50px',
+        triggerOnce: true
+    }), ref = _a.ref, inView = _a.inView;
     return (React.createElement(React.Fragment, null,
-        React.createElement("div", { className: 'wrap' },
+        React.createElement("div", { ref: ref, className: 'wrap' }, inView && (React.createElement("div", { className: "animate__animated animate__lightSpeedInLeft" },
             React.createElement("div", { className: 'title' },
                 React.createElement("h3", null, sysName)),
             React.createElement("div", null,
@@ -53,5 +65,5 @@ exports.AboutMyCareer = function (props) {
                 React.createElement("br", null),
                 React.createElement("p", null, third),
                 React.createElement("p", null, fourth),
-                React.createElement("p", null, five)))));
+                React.createElement("p", null, five)))))));
 };
