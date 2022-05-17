0a64f1417687a85fce2bf29fa05aba3270a93376
hiro881213 <nikep.godbard@gmail.com>
2022-05-17 22:45:33 +0900

    responsive for script

--- src/components/Script/ScriptMain.tsx
+++ src/components/Script/ScriptMain.tsx
@@ -1,10 +1,19 @@
+import MediaQuery from "react-responsive";
+import { Link } from 'react-router-dom';
+
 import { getUrl } from '../../CommonUtil'
 import { LogoInf } from '../Common/LogoInf'
 import { ScriptList } from './ScriptList';
 
+import story1 from '../../assets/scenario/scenario1/page1.jpg';
+import story2 from '../../assets/scenario/scenario2/page1.jpg';
+
+
 import classes from './scss/ScriptMain.module.scss';
 
 export const ScriptMain = () => {
+
+    const baseURL = "../"
 
     return (
         <>
@@ -13,13 +22,29 @@ export const ScriptMain = () => {
                     <LogoInf type = {1}></LogoInf>
                 </a>
             </header>
-
-            <main className={classes.mainClass}>
-                <label>脚本作品集</label>
-                <div className={classes.listScripts}>
-                    <ScriptList></ScriptList>
+            <MediaQuery query = '(min-width: 768px)'>
+                <main className={classes.mainClass}>
+                    <label>脚本作品集</label>
+                    <div className={classes.listScripts}>
+                        <ScriptList></ScriptList>
+                    </div>
+                </main>
+            </MediaQuery>
+            <MediaQuery query = '(max-width: 767px)'>
+                <div className= {classes.mainClass}>
+                    <label>脚本作品集</label>
+                    <div style = {{padding: "150px 0"}}>
+                        <Link to = {`${baseURL}detailScenario?story=1`}>
+                            <img src={story1} alt = {"scenario1"} className={classes.scriptImg} />
+                        </Link>
+                    </div>
+                    <div>
+                        <Link to = {`${baseURL}detailScenario?story=2`}>
+                            <img src={story2} alt = {"scenario2"} className={classes.scriptImg} />
+                        </Link>
+                    </div>
                 </div>
-            </main>
+            </MediaQuery>
         </>
     )
 }
--- src/components/Script/dist/ScriptMain.js
+++ src/components/Script/dist/ScriptMain.js
@@ -1,17 +1,32 @@
 "use strict";
 exports.__esModule = true;
 exports.ScriptMain = void 0;
+var react_responsive_1 = require("react-responsive");
+var react_router_dom_1 = require("react-router-dom");
 var CommonUtil_1 = require("../../CommonUtil");
 var LogoInf_1 = require("../Common/LogoInf");
 var ScriptList_1 = require("./ScriptList");
+var page1_jpg_1 = require("../../assets/scenario/scenario1/page1.jpg");
+var page1_jpg_2 = require("../../assets/scenario/scenario2/page1.jpg");
 var ScriptMain_module_scss_1 = require("./scss/ScriptMain.module.scss");
 exports.ScriptMain = function () {
+    var baseURL = "../";
     return (React.createElement(React.Fragment, null,
         React.createElement("header", { className: 'w-container' },
             React.createElement("a", { href: CommonUtil_1.getUrl() },
                 React.createElement(LogoInf_1.LogoInf, { type: 1 }))),
-        React.createElement("main", { className: ScriptMain_module_scss_1["default"].mainClass },
-            React.createElement("label", null, "\u811A\u672C\u4F5C\u54C1\u96C6"),
-            React.createElement("div", { className: ScriptMain_module_scss_1["default"].listScripts },
-                React.createElement(ScriptList_1.ScriptList, null)))));
+        React.createElement(react_responsive_1["default"], { query: '(min-width: 768px)' },
+            React.createElement("main", { className: ScriptMain_module_scss_1["default"].mainClass },
+                React.createElement("label", null, "\u811A\u672C\u4F5C\u54C1\u96C6"),
+                React.createElement("div", { className: ScriptMain_module_scss_1["default"].listScripts },
+                    React.createElement(ScriptList_1.ScriptList, null)))),
+        React.createElement(react_responsive_1["default"], { query: '(max-width: 767px)' },
+            React.createElement("div", { className: ScriptMain_module_scss_1["default"].mainClass },
+                React.createElement("label", null, "\u811A\u672C\u4F5C\u54C1\u96C6"),
+                React.createElement("div", { style: { padding: "150px 0" } },
+                    React.createElement(react_router_dom_1.Link, { to: baseURL + "detailScenario?story=1" },
+                        React.createElement("img", { src: page1_jpg_1["default"], alt: "scenario1", className: ScriptMain_module_scss_1["default"].scriptImg }))),
+                React.createElement("div", null,
+                    React.createElement(react_router_dom_1.Link, { to: baseURL + "detailScenario?story=2" },
+                        React.createElement("img", { src: page1_jpg_2["default"], alt: "scenario2", className: ScriptMain_module_scss_1["default"].scriptImg })))))));
 };
